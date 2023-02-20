b3198d533839175a37b0ea4e66b01657f6683adb
ivan-verbnyak <kadet717@ya.ru>
2023-02-20 12:44:26 +0500

    task19

---
+++ task19/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.Write("Input number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string s = a.ToString();
+char[] ar = s.ToCharArray();
+Array.Reverse(ar);
+s = new String(ar);
+int x = Convert.ToInt32(s);
+if ( a == x) 
+{
+    System.Console.WriteLine("the given number is a palindrome");
+}
+else 
+{
+    System.Console.WriteLine("the given number is not a palindrome");
+}
+//System.Console.WriteLine(x);
