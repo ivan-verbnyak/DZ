9ea6fcb13afdb877ef30f5bcf381fdf6d5a7e2b0
ivan-verbnyak <kadet717@ya.ru>
2023-02-20 12:57:30 +0500

    task15a

--- task15/Program.cs
+++ task15/Program.cs
@@ -2,22 +2,8 @@
 Console.Write("input number: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-switch (n)
-{
-    case 1: System.Console.WriteLine("no");
-        break;
-    case 2: System.Console.WriteLine("no");
-        break;
-    case 3: System.Console.WriteLine("no");
-        break;
-    case 4: System.Console.WriteLine("no");
-        break;
-    case 5: System.Console.WriteLine("no");
-        break;
-    case 6: System.Console.WriteLine("yes");
-        break;
-    case 7: System.Console.WriteLine("yes");
-        break;
-    default: System.Console.WriteLine("There is no such day of the week");
-        break;
-}
+if (n > 0 && n < 6) System.Console.WriteLine("Работаем");
+
+else if (n > 5 && n < 8) System.Console.WriteLine("Пора отдыхать");
+
+else System.Console.WriteLine("не такого дня недели");
