4ef34ea60519d7bb2e5d6932f089830e894b0cea
ivan-verbnyak <kadet717@ya.ru>
2023-02-28 09:04:54 +0500

    task25b

--- task25/Program.cs
+++ task25/Program.cs
@@ -9,6 +9,7 @@
 System.Console.Write("Введите число: ");
 double Number = Convert.ToInt32(Console.ReadLine());
 System.Console.Write("Введите степень возведения: ");
+
 double Degree = Convert.ToInt32(Console.ReadLine());
 
 double Result = Math.Pow(Number, Degree);
